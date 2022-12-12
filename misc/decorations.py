d7832267b6586994afd2ae0b5d37920d3c5f009f
ASF Codes <96375683+ASF007@users.noreply.github.com>
2022-12-12 15:18:34 +0400

    Add: all files

---
+++ misc/decorations.py
@@ -0,0 +1,36 @@
+"""
+This file stores variables containing the menu etc..
+"""
+
+ux = """
+            ===============================   
+            🏦 WELCOME TO NATIONAL BANK 🏦
+            ===============================   
+
+
+To proceed please select one of the options:
+
+1. Login & view your credentials
+2. Create account (for first time users)
+3. Delete account
+4. Purchase an item (WIP)
+5. Deposit
+6. Withdraw
+7. Exit
+"""
+
+data_ux = """
+
+            ==============================
+            🏧 NATIONAL BANK USER PORTAL 🏧
+            ==============================   
+
+
+NAME: {}
+BALANCE: {} OMR
+ITEMS (IN VAULT): {}
+
+ID: {}
+Time of Login: {}
+"""
+
