11457ace2d8d798b3172abec5cda3e37844a39d8
ASF Codes <96375683+ASF007@users.noreply.github.com>
2022-12-12 15:42:56 +0400

    Add: config file

---
+++ misc/config.py
@@ -0,0 +1,9 @@
+"""
+This file contains all the variables needed, inorder to connect to our database.
+
+"""
+
+HOST = "localhost"
+DB_NAME = "csproject" # name of the database created
+USER = "root" # in most cases its root
+PASSWORD = "your password" # password used whilst setting up mysql
