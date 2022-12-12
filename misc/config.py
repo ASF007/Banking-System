8d7ebd2f4b86f65fec03c374cc56233908bfcef9
ASF Codes <96375683+ASF007@users.noreply.github.com>
2022-12-12 15:42:15 +0400

    Delete config.py

--- misc/config.py
+++
@@ -1,9 +0,0 @@
-"""
-This file contains all the variables needed, inorder to connect to our database.
-
-"""
-
-HOST = "localhost"
-DB_NAME = "csproject" # name of the database created
-USER = "root" # in most cases its root
-PASSWORD = "aman" # password used whilst setting up mysql
